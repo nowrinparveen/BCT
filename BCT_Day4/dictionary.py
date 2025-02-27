c7e78886946e9732cec1b5b06028a2f87ab9698d
nowrinparveen <nowrinparveen365@gmail.com>
2025-02-27 15:25:48 +0530

    Add files via upload

---
+++ BCT_Day4/dictionary.py
@@ -0,0 +1,13 @@
+d1={
+"name":"noddy",
+"rollno.":"1",
+"class":"cse2",
+}
+
+print(d1.get("name"))
+print(d1.keys())
+print(d1.values())
+print(d1.items())
+print(d1.popitem())
+print(d1)
+
---
+++ BCT_Day4/set.py
@@ -0,0 +1,15 @@
+t={}
+print(type(t))
+s={2,9,45,32,1}
+print(type(s))
+s.add(7)
+print(s)
+s.remove(45)
+print(s)
+s.discard(2)
+print(s)
+s.update({3,4,5})
+print("the new set is=",s)
+
+
+
